1db974cf8d055d4c378f0f2413759768655dfeca
mrohith29 <mariyalarohith29@gmail.com>
2024-10-14 15:37:19 +0530

    add clock redirect

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,6 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
     redirects: {
         '/resume': '/resume.pdf',
+        '/clock': 'https://rohithmariyala.me/JS-clock/',
     }
 });
